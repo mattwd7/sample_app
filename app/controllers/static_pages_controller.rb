551acd7cfb2240f64ff6a237367b09eef95db265
Matthew Dick <mattwd7@gmail.com>
2012-09-13 10:38:00 -0700

    New routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,20 @@
 class StaticPagesController < ApplicationController
   def home
+  	respond_to do |format|
+  		format.html
+  	end
   end
 
   def help
+  	  respond_to do |format|
+  		format.html
+  	end
   end
 
   def about
+  	respond_to do |format|
+  		format.html
+  	end
   	
   end
 end
